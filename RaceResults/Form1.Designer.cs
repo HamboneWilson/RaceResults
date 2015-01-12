821b03484ad230c4a16fb1c1cf817dd1e5e3f3c4
Adam Carr <carr.adam@gmail.com>
2015-01-12 11:16:53 -0500

    Full Race Results Application

---
+++ RaceResults/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace RaceResults
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAgeGroup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRank = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colName,
+            this.colTime,
+            this.colAge,
+            this.colAgeGroup,
+            this.colRank});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 422);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            // 
+            // colTime
+            // 
+            this.colTime.HeaderText = "Time";
+            this.colTime.Name = "colTime";
+            // 
+            // colAge
+            // 
+            this.colAge.HeaderText = "Age";
+            this.colAge.Name = "colAge";
+            // 
+            // colAgeGroup
+            // 
+            this.colAgeGroup.HeaderText = "Age Group";
+            this.colAgeGroup.Name = "colAgeGroup";
+            this.colAgeGroup.Visible = false;
+            // 
+            // colRank
+            // 
+            this.colRank.HeaderText = "Rank";
+            this.colRank.Name = "colRank";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(143, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RACE RESULTS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(469, 490);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Race Results";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAgeGroup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRank;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RaceResults/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceResults
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadFile();
+            dataGridView1.Sort(colTime, ListSortDirection.Ascending);
+            SetRank();
+        }
+        private void ReadFile()
+        {
+            //Open a stream from the race results text and read it back
+            using (StreamReader sr = File.OpenText("RaceResults.txt"))
+            {
+                string line = "";
+                int counter = 0;
+                while ((line = sr.ReadLine()) != null)
+                //Add line from race results to the datagrid using WorkLine
+                {
+                    if (counter < 2)
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        WorkLine(line);
+                    }
+                }
+
+            }
+        }
+        //Function to handle actually parsing logic
+        private void WorkLine(string lineData)
+        {
+            //Break the columns into variables
+            string runnerName = (lineData.Substring(0, 8)).Trim();
+            int runnerTime = Convert.ToInt16((lineData.Substring(8, 8)).Trim());
+            int runnerAge = Convert.ToInt16((lineData.Substring(16)).Trim());
+            int runnerAgeGroup = 0;
+
+            //determine age group
+            if (runnerAge <= 15)
+            {
+                runnerAgeGroup = 1;
+            }
+            else if (runnerAge < 30)
+            {
+                runnerAgeGroup = 2;
+            }
+            else
+            {
+                runnerAgeGroup = 3;
+            }
+
+            //Write the results to the data grid
+
+            dataGridView1.Rows.Add(runnerName, runnerTime, runnerAge, runnerAgeGroup, 0);
+        }
+
+        //Function to determine rank for a list that has been sorted by time
+        private void SetRank()
+        {
+            //Store Group Rankings
+            int group1Rank = 1;
+            int group2Rank = 1;
+            int group3Rank = 1;
+            //Iterate through each row checking age and updating rank, while incrementing the appropriate sotred ranking
+            for (int rows = 0; rows < dataGridView1.Rows.Count; rows++)
+            {
+                if (dataGridView1.Rows[rows].Cells["colAgeGroup"].Value.Equals(1))
+                {
+                    dataGridView1.Rows[rows].Cells["colRank"].Value = group1Rank;
+                    group1Rank++;
+                }
+                else if (dataGridView1.Rows[rows].Cells["colAgeGroup"].Value.Equals(2))
+                {
+                    dataGridView1.Rows[rows].Cells["colRank"].Value = group2Rank;
+                    group2Rank++;
+                }
+                else
+                {
+                    dataGridView1.Rows[rows].Cells["colRank"].Value = group3Rank;
+                    group3Rank++;
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
